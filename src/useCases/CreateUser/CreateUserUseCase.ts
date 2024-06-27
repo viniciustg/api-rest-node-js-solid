ac07ffdfefffcb04aeb5305a0d4190c01d481232
viniciustarga <devopstarga@outlook.com>
2024-06-27 08:18:04 -0300

    Creating dto and service rules to save

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,12 +1,21 @@
-export class CreateUserUseCase {
-    /**
-     *
-     */
-    constructor() {
-                
-    }
-    
-    async execute() {
+import { User } from "../../entities/User";
+import { IUserRepository } from "../../repositories/IUserRepository"
+import { ICreateUserDTO } from "./CreateUserDTO"
 
+export class CreateUserUseCase {
+    constructor(
+        private usersRepository: IUserRepository 
+    ) { }
+    
+    async execute(data: ICreateUserDTO) {
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+        
+        if (userAlreadyExists) {
+            throw new Error("User already exists.");
+        }
+    
+        const user = new User(data);
+
+        await this.usersRepository.save(user);    
     }
 }
